e39a7d82db151b46a4f641149b5dd68317b060e9
TripQuestAdmin <enquestes0@gmail.com>
2025-07-07 19:16:59 +0200

    Primer commit de TripQuest

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>¡Hola TripQuest!</h1>
+      <p>React + Vite funcionando 🎉</p>
+    </div>
+  );
+}
+
+export default App;
+
